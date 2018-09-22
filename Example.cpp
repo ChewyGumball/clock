ed9b05037006c6561e6b778805913d32be356479
Ben Dunkin <chewygumbal@gmail.com>
2018-09-22 14:17:42 -0700

    Fleshed out example code

--- Example.cpp
+++ Example.cpp
@@ -3,43 +3,87 @@
 
 #include "Clock.h"
 
+void printClocks(Time::Clock& fastClock, Time::Clock& slowClock);
+void printTimers(Time::Timer& fastTimer, Time::Timer& slowTimer);
+
 int main() {
     using Time::Clock;
     using Time::Timer;
 
     constexpr Clock::Seconds SLEEP_TIME = std::chrono::seconds(1);
+    constexpr float TIME_SCALE = 1.0f / 3.0f;
 
     Clock clock;
+    Clock slowClock(TIME_SCALE);
     Timer timer(&clock);
+    Timer slowTimer(&slowClock);
+
+    std::cout << "Seconds per tick: " << SLEEP_TIME.count() << "\n";
+    std::cout << "Time Dilation Factor: " << TIME_SCALE << std::endl;
 
     for(int i = 0; i < 10; i++) {
         std::this_thread::sleep_for(SLEEP_TIME);
         clock.tick();
+        slowClock.tick();
 
         Clock::Seconds tickedSeconds = clock.tickedTime();
         Clock::Seconds elapsedSeconds = clock.totalElapsedTime();
 
-        std::cout << "Iteration " << i + 1 << ": Tick Time = " << tickedSeconds.count() << "s Elapsed Time = " << elapsedSeconds.count() << "s\n";
+        std::cout << "Tick " << i + 1 << "\n";
+        printClocks(clock, slowClock);
 
         if(i == 3) {
             timer.stop();
-            Clock::Seconds timerElapsedSeconds = timer.elapsedTime();
-            std::cout << "Timer Stopped: Elapsed Time = " << timerElapsedSeconds.count() << "s\n";
+            slowTimer.stop();
+            std::cout << "Timer Stopped:\n";
+            printTimers(timer, slowTimer);
         }
 
         if(i == 5) {
+            std::cout << "Timer Started:\n";
+            printTimers(timer, slowTimer);
             timer.start();
-            Clock::Seconds timerElapsedSeconds = timer.elapsedTime();
-            std::cout << "Timer Started: Elapsed Time = " << timerElapsedSeconds.count() << "s\n";
+            slowTimer.start();
         }
 
         if(i == 7) {
-            Clock::Seconds timerElapsedSeconds = timer.elapsedTime();
-            std::cout << "Timer Resetting: Elapsed Time = " << timerElapsedSeconds.count() << "s\n";
+            std::cout << "Timer Resetting:\n";
+            printTimers(timer, slowTimer);
             timer.reset();
+            slowTimer.reset();
         }
     }
 
-    Clock::Seconds timerElapsedSeconds = timer.elapsedTime();
-    std::cout << "Timer Elapsed Time = " << timerElapsedSeconds.count() << "s\n";
+    std::cout << "Final Timer Times:\n";
+    printTimers(timer, slowTimer);
+}
+
+void printClocks(Time::Clock& fastClock, Time::Clock& slowClock)
+{
+    using Time::Clock;
+
+    Clock::Seconds tickedSeconds = fastClock.tickedTime();
+    Clock::Seconds elapsedSeconds = fastClock.totalElapsedTime();
+
+    Clock::Seconds slowTickedSeconds = slowClock.tickedTime();
+    Clock::Seconds slowElapsedSeconds = slowClock.totalElapsedTime();
+
+    std::cout << "\t Fast Clock: Tick Time = " << tickedSeconds.count() << "s Elapsed Time = " << elapsedSeconds.count() << "s\n";
+    std::cout << "\t Slow Clock: Tick Time = " << slowTickedSeconds.count() << "s Elapsed Time = " << slowElapsedSeconds.count() << "s\n";
+
+    std::cout << std::flush;
+}
+
+void printTimers(Time::Timer& fastTimer, Time::Timer& slowTimer)
+{
+    using Time::Clock;
+    using Time::Timer;
+
+    Clock::Seconds timerElapsedSeconds = fastTimer.elapsedTime();
+    Clock::Seconds slowTimerElapsedSeconds = slowTimer.elapsedTime();
+
+    std::cout << "\t Fast Timer: Elapsed Time = " << timerElapsedSeconds.count() << "s\n";
+    std::cout << "\t Slow Timer: Elapsed Time = " << slowTimerElapsedSeconds.count() << "s\n";
+    
+    std::cout << std::flush;
 }
--- README.md
+++ README.md
@@ -1,15 +1,63 @@
-# clock
+# Clock
 A simple, header only library for keeping track of time based on std::chrono.
 
-#Features
-##Clock
-###Ticking
+# Features
+## Clock
+### Ticking
 A `Clock` only advances time when its `tick()` function is called. The time between the previous two `tick()` calls can be retrieved using the `tickedTime()` function.
-###Pausing
+### Pausing
 A `Clock` can be paused by calling its `pause()` function. Calls to `tick()` will not advance time while a `Clock` is paused, and `tickedTime()` will return a duration of zero time. A `Clock` can be resumed by calling its `resume()` function.
-###Time Dilation
+### Time Dilation
 A `Clock` can be run faster or slower than wall time by using the `timeScaling` parameter in the `Clock` constructor. The returned values from both `tickedTime()` and `totalElapsedTime()` will be scaled by the value of `timeScaling`.
-###Clock::Seconds vs std::chrono::seconds
+### `Clock::Seconds` vs `std::chrono::seconds`
 `Clock::Seconds` can represent fractional seconds while `std::chrono::seconds` can not.
-##Timer
+## Timer
 A `Timer` keeps track of the elapsed time of a `Clock`. Multiple `Timer`s can be tracking time of the same `Clock`. A `Timer`'s elapsed time is scaled by the same amount of the `Clock` is its tracking.
+# Example Output
+The output of the example code provided is:
+```
+Seconds per tick: 1
+Time Dilation Factor: 0.333333
+Tick 1
+         Fast Clock: Tick Time = 1.00172s Elapsed Time = 1.00172s
+         Slow Clock: Tick Time = 0.333906s Elapsed Time = 0.333906s
+Tick 2
+         Fast Clock: Tick Time = 1.00327s Elapsed Time = 2.00499s
+         Slow Clock: Tick Time = 0.334424s Elapsed Time = 0.66833s
+Tick 3
+         Fast Clock: Tick Time = 1.00316s Elapsed Time = 3.00815s
+         Slow Clock: Tick Time = 0.334386s Elapsed Time = 1.00272s
+Tick 4
+         Fast Clock: Tick Time = 1.00284s Elapsed Time = 4.01099s
+         Slow Clock: Tick Time = 0.33428s Elapsed Time = 1.337s
+Timer Stopped:
+         Fast Timer: Elapsed Time = 4.01099s
+         Slow Timer: Elapsed Time = 1.337s
+Tick 5
+         Fast Clock: Tick Time = 1.00425s Elapsed Time = 5.01524s
+         Slow Clock: Tick Time = 0.334751s Elapsed Time = 1.67175s
+Tick 6
+         Fast Clock: Tick Time = 1.00375s Elapsed Time = 6.01899s
+         Slow Clock: Tick Time = 0.334583s Elapsed Time = 2.00633s
+Timer Started:
+         Fast Timer: Elapsed Time = 4.01099s
+         Slow Timer: Elapsed Time = 1.337s
+Tick 7
+         Fast Clock: Tick Time = 1.00498s Elapsed Time = 7.02397s
+         Slow Clock: Tick Time = 0.334993s Elapsed Time = 2.34132s
+Tick 8
+         Fast Clock: Tick Time = 1.00334s Elapsed Time = 8.02731s
+         Slow Clock: Tick Time = 0.334448s Elapsed Time = 2.67577s
+Timer Resetting:
+         Fast Timer: Elapsed Time = 6.01931s
+         Slow Timer: Elapsed Time = 2.00644s
+Tick 9
+         Fast Clock: Tick Time = 1.00467s Elapsed Time = 9.03198s
+         Slow Clock: Tick Time = 0.33489s Elapsed Time = 3.01066s
+Tick 10
+         Fast Clock: Tick Time = 1.003s Elapsed Time = 10.035s
+         Slow Clock: Tick Time = 0.334334s Elapsed Time = 3.345s
+Final Timer Times:
+         Fast Timer: Elapsed Time = 2.00767s
+         Slow Timer: Elapsed Time = 0.669224s
+```
